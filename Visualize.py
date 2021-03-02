af5b314e06216a7686dbe267bd04d24fb48f48a5
abhinavgairola <abhinavgairola@Abhinavs-MacBook-Pro.local>
2021-03-02 12:15:19 -0500

    Added Gensim stopword removal

--- Visualize.py
+++ Visualize.py
@@ -1,6 +1,7 @@
 import json 
 import plotly.graph_objects as go
 import collections
+import gensim
 
 class Read:
     """
@@ -42,7 +43,9 @@ class Visualize:
 
 
     def bar(self, datax,datay, title_x, title_y,plot_title,legend,**args):
-        fig = go.Figure(go.Bar(x=datax,y=datay,orientation='h',marker_color='darkred'))
+        colors = ['aliceblue',  'aqua', 'aquamarine', 'darkturquoise']
+        fig = go.Figure(go.Bar(x=datax,y=datay,orientation='h',marker={'color': datax,
+                                               'colorscale': 'rainbow'}))##marker_color='darkred'))
         fig.update_layout(
             title=plot_title,
             xaxis_title=title_x,
@@ -51,20 +54,26 @@ class Visualize:
             font=dict(
                 family="Times New Roman",size=18,
                 color='black'),
-                xaxis={'categoryorder':'category ascending'})
+                xaxis={'categoryorder':'category ascending'}
+                
+                ,yaxis=dict(autorange='reversed'))
         return fig
 
 if __name__ =='__main__':    
     Data = Read()
-    data = Data.read_json(r'132021/ABC_news.json') 
+    data = Data.read_json(r'232021/ABC_news.json') 
     plot = Visualize()
     All_figures=[]
     for key, value in data.items():
         if value is not None:
             
-            Article = ("".join(value)).replace('"','').replace('\'','').replace(',','').replace('.','').split()
+            
+            temp_2 = "".join(value).replace('"','').replace('\'','').replace(',','').replace('.',' ').replace('--','').replace('———',' ').lower()
+            temp_2 = gensim.parsing.preprocessing.remove_stopwords(temp_2).upper()
+            Article = (temp_2.split())
+
             print("This is the article {}".format(Article))
-            counts = (collections.Counter(Article).most_common(50))
+            counts = (collections.Counter(Article).most_common(10))
             #print(counts)
             y =[]
             x=[]
@@ -75,5 +84,5 @@ if __name__ =='__main__':
     
         
         All_figures.append(plot.bar(x,y,'Counts','Words','Word Counts','Legend'))
-    (All_figures[60]).show()
+    (All_figures[20]).show()
     
